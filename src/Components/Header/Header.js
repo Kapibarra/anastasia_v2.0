9e128a611d544176df06597407eaa422805c7014
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-04-14 14:56:25 +0300

    added burger menu

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,19 +3,35 @@ import classes from "./Header.module.css";
 import logo from "../../img/logo.svg"
 import logo2 from "../../img/logo2text.svg"
 import Navigation from "../../Ui/NavLink/Navigation";
+import Burger from "../../Ui/BurgerMenu/Burger";
 
 const Header = () => {
-  return (
-    <div className={classes.Header}>
+  if (window.screen.width >= 968) {
+    return (
+      <div className={classes.Header}>
+        <div className={classes.Header__Logo}>
+        <img src={logo} alt=""></img>
+        <img src={logo2} alt=""></img>
+        </div>
+        <div className={classes.Header__Nav}>
+          <Navigation />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className={classes.Header}>
       <div className={classes.Header__Logo}>
       <img src={logo} alt=""></img>
       <img src={logo2} alt=""></img>
       </div>
       <div className={classes.Header__Nav}>
-        <Navigation />
+        <Burger />
       </div>
     </div>
-  );
+    )
+  }
+
 };
 
 export default Header;
---
+++ src/Ui/BurgerMenu/Burger.js
@@ -0,0 +1,85 @@
+import { slide as Menu } from 'react-burger-menu'
+import React from 'react'
+import {
+    BrowserRouter ,
+    Link
+  } from "react-router-dom";
+  
+  var styles = {
+    bmBurgerButton: {
+      position: 'fixed',
+      width: '36px',
+      height: '30px',
+      right: '36px',
+      top: '36px'
+    },
+    bmBurgerBars: {
+      background: '#373a47'
+    },
+    bmBurgerBarsHover: {
+      background: '#a90000'
+    },
+    bmCrossButton: {
+      height: '24px',
+      width: '24px'
+    },
+    bmCross: {
+      background: '#bdc3c7'
+    },
+    bmMenuWrap: {
+      position: 'fixed',
+      height: '100%',
+      top: '0',
+      background: 'rgba(#497577, 0.8)'
+    },
+    bmMenu: {
+      background: '#373a47',
+      padding: '2.5em 1.5em 0',
+      fontSize: '1.15em',
+      display:'block',
+      background: 'rgba(73, 117, 119, 0.9)'
+    },
+    bmMorphShape: {
+      fill: '#373a47'
+    },
+    bmItemList: {
+      color: '#b8b7ad',
+      padding: '0.8em',
+      
+    },
+    bmItem: {
+        display:'block',
+        padding: '5px'
+    },
+    bmOverlay: {
+        position: 'fixed',
+        top:'0',
+        width: '100%',
+        height: '100%',
+        background: 'rgba(0, 0, 0, 0.3)',
+        opacity: 1,
+      background: 'rgba(0, 0, 0, 0.3)',
+      transition: 'all 0.5s ease 0s',
+      transform: 'translate3d(100%, 0px, 0px)',
+    }
+  }
+
+class Burger extends React.Component {
+  showSettings (event) {
+    event.preventDefault();
+  }
+
+  render () {
+    // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
+    return (
+      <Menu right styles={ styles }>
+      <Link to="/">Главная</Link>
+              <Link to="/about">Кто я</Link>
+              <Link to="/services">Услуги</Link>
+              <Link to="/rewiews">Отзывы</Link>
+              <Link to="/contacts">Контакты</Link>
+      </Menu>
+    );
+  }
+}
+export default Burger
