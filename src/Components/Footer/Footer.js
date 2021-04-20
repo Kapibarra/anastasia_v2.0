0741c9edf2aa9210fa1d6321b0ac4670f29eeb1b
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-04-20 15:00:35 +0300

    fixes

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,7 @@
 import classes from "./Footer.module.css";
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
 import { SocialIcon } from 'react-social-icons';
-import sociallogo1 from "../../img/whatsapp_logo.png"
-import sociallogo2 from "../../img/telegram_logo2.png"
-import sociallogo3 from "../../img/profi-logo.png"
-import sociallogo4 from "../../img/youtube_logo.png"
+
 
 const Footer = (props) => {
   return (
