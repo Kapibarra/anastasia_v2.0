debc57ee72bf7f716e013f6f170911c1a969e537
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-04-22 14:16:20 +0300

    fixes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import classes from "./Header.module.css";
 import logo from "../../img/logo.svg"
-import logo2 from "../../img/logo2text.svg"
 import Navigation from "../../Ui/NavLink/Navigation";
 import Burger from "../../Ui/BurgerMenu/Burger";
 
@@ -27,7 +26,10 @@ const Header = () => {
       <div className={classes.Header}>
       <div className={classes.Header__Logo}>
       <img src={logo} alt=""></img>
-      <img src={logo2} alt=""></img>
+      <div className={classes.Header__Logo_text}>
+          <p className={classes.Header__Logo_text1}>ANASTASIA R</p>
+          <p className={classes.Header__Logo_text2}>PSYCHOTERAPY</p>
+        </div>
       </div>
       <div className={classes.Header__Nav}>
         <Burger />
