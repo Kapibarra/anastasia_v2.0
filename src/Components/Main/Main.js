0ba0cd0fad2dfb454cdb3b5d8bd1d61edd575c56
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-07-28 23:23:08 +0300

    added frame animations

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -14,12 +14,20 @@ import {
   pageAnimation,
   photoAnim,
   titleAnim,
+  sliderContainer,
+  slider
 } from "../../Ui/Animations";
 
 const Main = () => {
   return (
     <div>
       <div className={classes.MainWrapper}>
+      <motion.div variants={sliderContainer}>
+    <Frame1 variants={slider}></Frame1>
+    <Frame2 variants={slider}></Frame2>
+    <Frame3 variants={slider}></Frame3>
+    <Frame4 variants={slider}></Frame4>
+    </motion.div>
         <div className={classes.MainContent}>
           <div className={classes.MainContentText}>
             <motion.p
@@ -106,4 +114,23 @@ const Main = () => {
     </div>
   );
 };
+// Frame Animation
+const Frame1 = styled(motion.div)`
+position: fixed;
+left:0;
+top: 10%;
+width: 100%;
+height: 100vh;
+background: #fffebf;
+z-index: 2;
+`
+const Frame2 = styled(Frame1)`
+background: #ff8efb;
+`
+const Frame3 = styled(Frame1)`
+background: #8ed2ff;
+`
+const Frame4 = styled(Frame1)`
+background: #8effa0;
+`
 export default Main;
